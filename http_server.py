ad3f281179a4a0452837752d5e350ab10cf9458d
jancvek <jan.cvek@gmail.com>
2020-02-15 17:53:25 +0100

    init

--- http_server.py
+++ http_server.py
@@ -27,6 +27,8 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print(self.path)
 
+        # print(self.path)
+        # rootdir = 'c:/xampp/htdocs/' #file location  
         try:  
             # if self.path.endswith('.html'): 
             if self.path.endswith('/'):   
@@ -63,6 +65,50 @@ class Handler(BaseHTTPRequestHandler):
                 self.wfile.write(fileData.encode('utf-8'))  
                 f.close()  
                 return
+
+            elif self.path.endswith('/api/test'):
+                self.send_response(200)
+                # self.send_header('Content-type','text/html')
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                
+                # Send the html message
+                # self.wfile.write(b'Hello, world!')
+
+                # data_list = []
+
+                # conn = jan_sqlite.create_connection(r"data.db")
+                # with conn:
+                #     rows = jan_sqlite.get_data_all(conn)
+
+                #     for row in rows:
+                #         d = {
+                #             "id": row[0],
+                #             "text": row[1]
+                #         }
+                #         data_list.append(d)
+
+
+                # data_list = []
+                
+                # mes1 = {
+                #     "name": "John",
+                #     "age": 30
+                # }
+                # mes2 = {
+                #     "name": "Ana",
+                #     "age": 22
+                # }
+
+                # data_list.append(mes1)
+                # data_list.append(mes2)
+
+                # mStr = json.dumps(message)
+                # mStr = json.dumps(data_list)
+                mStr = json.dumps({"test":"dela"})
+                mBin = mStr.encode('utf-8')
+
+                self.wfile.write(mBin)
 
         except IOError:  
             self.send_error(404, 'file not found')  
